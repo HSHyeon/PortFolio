08968cf9114eb84a243496e9a9f4a8a3f1ac35d5
HSHyeon <hshyeon86@gmail.com>
2023-10-21 01:36:43 +0900

    이미지 슬라이드 해결

--- HOME/script.js
+++ HOME/script.js
@@ -1,13 +1,5 @@
-const quickIconContainer = document.querySelector(".quick-icons");
-
-
-const home = document.querySelector(".home");
-const homeOptions = {
-  threshold: 0.8,
-};
-
 const images = document.querySelectorAll('.slider span');
-const sliderContainer = document.querySelector('slider-container');
+const sliderContainer = document.querySelector('.slider-container');
 const slider = document.querySelector('.slider');
 const prevBtn = document.querySelector('.leftBtn');
 const nextBtn = document.querySelector('.rightBtn');
@@ -18,7 +10,7 @@ const imgSize = images[0].clientWidth;
 slider.style.transform = `translateX(${-imgSize}px)`;
 
 prevBtn.addEventListener('click',()=>{
-    if( current <= 0) return;
+    if(current <= 0) return;
     slider.style.transition = '400ms ease-in-out transform';
     current--;
     slider.style.transform = `translateX(${-imgSize * current}px)`;
@@ -44,47 +36,35 @@ slider.addEventListener('transitionend', ()=> {
     }
 })
 
+const next = () => {
+  if (current >= images.length - 1) return;
+  slider.style.transition = '400ms ease-in-out transform';
+  current++;
+  slider.style.transform = `translateX(${-imgSize * current}px)`;
 
+  for (let i = 0; i < dots.length; i++) {
+      if (dots[i].dataset.index == current) {
+          dots[i].classList.add('active');
+      } else if (current === 4) {
+          dots[i].classList.remove('active');
+          dots[0].classList.add('active');
+      }
+      else {
+          dots[i].classList.remove('active');
+      }
+  }
+}
 
-const handleHome = (entries) => {
-    if (entries[0].isIntersecting) {
-      quickIconContainer.classList.add("show-off");
-    } else {
-      quickIconContainer.classList.remove("show-off");
-    }
-  };
+let auto = setInterval(next, 3000);
 
-  const homeIcon = icons[0].firstChild;
-  homeIcon.addEventListener("click", () => {
-    icons.forEach((icon) => icon.classList.remove("icon-active"));
-  });
-  
-  const options = {
-    root: null, //default : viewport .. 부모컨테이너를 지정해줄 수 있다.
-    rootMargin: `0px`, //마진을 주면, 내가 보는 화면보다 기준영역이 그 만큼 더 늘어나게된다.
-    threshold: 0.5, //얼마만큼 보여져야 콜백함수를 호출할지 결정  0~1 줄 수 있다.
-  };
-  
-  const callback = (entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        switch (entry.target.id) {
-          case "about__traveling":
-            icons.forEach((icon) => icon.classList.remove("icon-active"));
-            icons[1].classList.add("icon-active");
-            break;
-          case "about__major":
-            icons.forEach((icon) => icon.classList.remove("icon-active"));
-            icons[2].classList.add("icon-active");
-            break;
-          default:
-            icons.forEach((icon) => icon.classList.remove("icon-active"));
-            break;
-        }
-      } 
-    });
-  };
+slider.addEventListener('mouseleave', ()=>{
+    auto = setInterval(next, 3000)
+})
 
-const observer = new IntersectionObserver(callback, options);
-sections.forEach((section) => observer.observe(section)); //section들을 관찰자가 관찰하도록 명령
-  
+slider.addEventListener('mouseenter', ()=>{
+    clearInterval(auto)
+})
+
+nextBtn.addEventListener('click', () => {
+  next()
+})
