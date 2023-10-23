6315c203da9b0a288f3a6e202f6236bf18e7f861
HSHyeon <hshyeon86@gmail.com>
2023-10-23 10:44:22 +0900

    [create board]local storage 추가

--- HOME/script.js
+++ HOME/script.js
@@ -10,15 +10,19 @@ const imgSize = images[0].clientWidth;
 slider.style.transform = `translateX(${-imgSize}px)`;
 
 prevBtn.addEventListener('click',()=>{
+
+  console.log(current);
     if(current <= 0) return;
-    slider.style.transition = '400ms ease-in-out transform';
+    slider.style.transition = '600ms ease-in-out transform';
     current--;
     slider.style.transform = `translateX(${-imgSize * current}px)`;
 })
 
 nextBtn.addEventListener('click',()=>{
+
+  console.log(current);
     if( current >= images.length -1 ) return;
-    slider.style.transition = '400ms ease-in-out transform';
+    slider.style.transition = '600ms ease-in-out transform';
     current++;
     slider.style.transform = `translateX(${-imgSize * current}px)`;
 })
@@ -27,32 +31,21 @@ slider.addEventListener('transitionend', ()=> {
     if(images[current].classList.contains('first-img')){
         slider.style.transition = 'none';
         current = images.length - 2;
-        slider.style.transform = `translateX(${-imgSize * current}px)`;
+       // slider.style.transform = `translateX(${-imgSize * current}px)`;
     }
     if(images[current].classList.contains('last-img')){
         slider.style.transition = 'none';
-        current = images.length - current;
-        slider.style.transform = `translateX(${-imgSize * current}px)`;
+        current = images.length - current ;
+      //  slider.style.transform = `translateX(${-imgSize * current}px)`;
     }
 })
 
 const next = () => {
   if (current >= images.length - 1) return;
-  slider.style.transition = '400ms ease-in-out transform';
+  slider.style.transition = '600ms ease-in-out transform';
   current++;
   slider.style.transform = `translateX(${-imgSize * current}px)`;
-
-  for (let i = 0; i < dots.length; i++) {
-      if (dots[i].dataset.index == current) {
-          dots[i].classList.add('active');
-      } else if (current === 4) {
-          dots[i].classList.remove('active');
-          dots[0].classList.add('active');
-      }
-      else {
-          dots[i].classList.remove('active');
-      }
-  }
+  
 }
 
 let auto = setInterval(next, 3000);
@@ -68,3 +61,50 @@ slider.addEventListener('mouseenter', ()=>{
 nextBtn.addEventListener('click', () => {
   next()
 })
+
+const list = document.querySelector('.memos');
+
+const input = document.getElementById('board_input');
+const submit= document.getElementById('submit');
+let str = localStorage.getItem('memo');
+
+if(str){
+  let arr = str.split(" ");
+  for(let i=0;i<arr.length;i++)
+  {
+    if(arr[i] != "null"){
+      const html = `<li class="list-group-item ">
+  <span>${arr[i]}</span>
+  <i class="far fa-trash-alt delete"></i>
+</li>`
+  list.innerHTML += html;
+}
+}}
+
+
+
+submit.addEventListener('click', e =>{
+  e.preventDefault();
+  const memo = input.value;
+
+  console.log(str);
+  if(memo.length){
+   
+    saveMemo(memo);   
+    input.value = '';
+    str = str + " " + memo;
+    localStorage.setItem('memo', str);
+ 
+}
+
+
+})
+
+const saveMemo = memotext => {
+  const html = `<li class="list-group-item ">
+  <span>${memotext}</span>
+  <i class="far fa-trash-alt delete"></i>
+</li>`
+  list.innerHTML += html;
+}
+
